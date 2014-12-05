54a206e446fffb4bf6a3379f1544b4e7fbf9e5c8
Diego Forero <gollum23@gmail.com>
2014-12-05 07:35:05 -0500

    Create views

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User, Group
+#from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Vista del API que permite ver o editar los usuarios
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Vista del API que permite ver o editar los grupos
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
