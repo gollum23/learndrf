bd707bee569ff290e9f84fc8331b7fd7c0a20c66
Diego Forero <gollum23@gmail.com>
2014-12-05 09:04:29 -0500

    Urls for tutorial-1

--- tutorial_1/urls.py
+++ tutorial_1/urls.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'gollum23'
 __date__ = '12/5/14'
+from django.conf.urls import url, patterns
+
+urlpatterns = patterns(
+    'tutorial_1.views',
+    url(r'^$', 'snippet_list'),
+    url(r'^(?P<pk>[0-9]+)/$', 'snippet_detail')
+)
