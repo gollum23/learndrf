5ea968dc496e1a99c2e4a45df8ab11ffacb4e90c
Diego Forero <gollum23@gmail.com>
2014-12-05 07:51:32 -0500

    Register urls

--- learndrf/urls.py
+++ learndrf/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^', include('quickstart.urls')),
+    url(r'^tutorial-1/', include('tutorial_1.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
