1c82aa06710b0acfe3953d1ebf7caa6ee4cf84b6
Diego Forero <gollum23@gmail.com>
2014-12-05 08:36:32 -0500

    Refactoring code for use ModelSerializer

--- tutorial_1/serializers.py
+++ tutorial_1/serializers.py
@@ -1,48 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'gollum23'
 __date__ = '12/5/14'
-from django.forms import widgets
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
-    # Definición de campos que son serializados y deserializados
-    pk = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(
-        required=False,
-        allow_blank=True,
-        max_length=100
-    )
-    code = serializers.CharField(
-        style={'type': 'textarea'}
-    )
-    linenos = serializers.BooleanField(
-        required=False
-    )
-    language = serializers.ChoiceField(
-        choices=LANGUAGE_CHOICES,
-        default='python'
-    )
-    style = serializers.ChoiceField(
-        choices=STYLE_CHOICES,
-        default='friendly'
-    )
+class SnippetSerializer(serializers.ModelSerializer):
 
-    def create(self, validated_data):
-        """
-        Crear y regresar una nueva instancia de Snippet, luego de validar los datos
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Actualizar y regresar una instancia existente de Snippet, luego de validar los datos
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data('code', instance.code)
-        instance.linenos = validated_data('linenos', instance.linenos)
-        instance.language = validated_data('language', instance.language)
-        instance.style = validated_data('style', instance.style)
-        instance.save()
-        return instance
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
