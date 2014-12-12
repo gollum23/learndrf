bb3df20b04e3a5a59cc796413e4bf771545d167b
Diego Forero <gollum23@gmail.com>
2014-12-12 07:33:08 -0500

    Creating and endpoint for the highlighted snippets

--- tutorial_1/urls.py
+++ tutorial_1/urls.py
@@ -5,12 +5,14 @@ from django.conf.urls import url, patterns, include
 # Import format suffix
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetList, SnippetDetail, UserList, UserDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail, SnippetHighlight
 
 urlpatterns = patterns(
     '',
-    url(r'^$', SnippetList.as_view()),
+    # url(r'^$', SnippetList.as_view()),
+    url(r'^$', 'tutorial_1.views.api_root'),
     url(r'^(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
+    url(r'^(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view()),
     url(r'^users/$', UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view()),
 
--- tutorial_1/views.py
+++ tutorial_1/views.py
@@ -2,8 +2,9 @@
 __author__ = 'gollum23'
 __date__ = '12/5/14'
 from django.contrib.auth.models import User
-from rest_framework import permissions
 from rest_framework import generics
+from rest_framework import permissions
+from rest_framework import renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -40,6 +41,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
