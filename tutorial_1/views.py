aee828287937c605a3887ff4caee2fa52108271e
Diego Forero <gollum23@gmail.com>
2014-12-12 07:26:41 -0500

    Creating and endpoint for the root of our api

--- tutorial_1/views.py
+++ tutorial_1/views.py
@@ -4,10 +4,21 @@ __date__ = '12/5/14'
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
