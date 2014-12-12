0198c2759cb90bc7887cd7d6fce2090cf5117311
Diego Forero <gollum23@gmail.com>
2014-12-12 08:07:43 -0500

    Binding ViewSets to URLs explicitly

--- tutorial_1/urls.py
+++ tutorial_1/urls.py
@@ -4,17 +4,42 @@ __date__ = '12/5/14'
 from django.conf.urls import url, patterns, include
 # Import format suffix
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
 
-from .views import SnippetList, SnippetDetail, UserList, UserDetail, SnippetHighlight
+from .views import SnippetViewSet, UserViewSet, api_root
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrive',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = patterns(
     '',
     url(r'^$', 'tutorial_1.views.api_root'),
-    url(r'^snippets/$', SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name='user-detail'),
+    url(r'^snippets/$', snippet_list, name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
