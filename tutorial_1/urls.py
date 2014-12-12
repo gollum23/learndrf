e173175aef2b64704aac35298a5819aa16633b70
Diego Forero <gollum23@gmail.com>
2014-12-12 07:36:39 -0500

    fix urls

--- tutorial_1/urls.py
+++ tutorial_1/urls.py
@@ -9,11 +9,11 @@ from .views import SnippetList, SnippetDetail, UserList, UserDetail, SnippetHigh
 
 urlpatterns = patterns(
     '',
-    # url(r'^$', SnippetList.as_view()),
     url(r'^$', 'tutorial_1.views.api_root'),
-    url(r'^(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
-    url(r'^(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view()),
-    url(r'^users/$', UserList.as_view()),
+    url(r'^snippets/$', SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', SnippetHighlight.as_view()),
+    url(r'^users/$', UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view()),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
